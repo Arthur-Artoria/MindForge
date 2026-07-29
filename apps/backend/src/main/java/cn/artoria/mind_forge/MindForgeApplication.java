4a968494f2f99a1dacfbc65136869cd8fa671b3b
Artoria Pendragon <LArtoria@hotmail.com>
2026-07-29 18:32:09 +0800

    feat: 初始化 Spring Boot 项目

---
+++ apps/backend/src/main/java/cn/artoria/mind_forge/MindForgeApplication.java
@@ -0,0 +1,13 @@
+package cn.artoria.mind_forge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindForgeApplication.class, args);
+	}
+
+}
---
+++ apps/backend/src/test/java/cn/artoria/mind_forge/MindForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.artoria.mind_forge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
